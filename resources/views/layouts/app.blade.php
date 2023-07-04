a8050cc61e0243d6cec4f847e4aa7e076be1a6a1
Agustín Ferreiroa González <40137464+agusvigo@users.noreply.github.com>
2023-07-04 15:37:27 +0200

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="robots" content="noindex, nofollow">
         <title>Devstagram - @yield('titulo')</title>
+        <link rel="shortcut icon" href="{{ asset('img') . 'favicon.png' }}" type="image/png">
         @stack('styles')
         @vite('resources/css/app.css')
         @livewireStyles
